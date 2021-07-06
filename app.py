d4789900370b435d9881757e078c7e5054392d3a
unknown <tomasz.blachowiak@gmail.com>
2021-07-06 15:45:40 +0200

    commit

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
+import json
+from urllib.request import urlopen
 
 app=Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("home.html")
+    response = urlopen('https://hydropoinc-greenhouse-api.azurewebsites.net/api/readings/GetAllReadings?')
+    data_json = json.loads(response.read())
+    temperature1 = [d["temperature1"] for d in data_json if "temperature1" in d]
+    ts = [d['_ts'] for d in data_json if '_ts' in d]
+    print(temperature1)
+    print(ts)
+    return render_template("home.html", temperature1 = temperature1, ts = ts)
 
 @app.route('/charts/')
 def charts():
